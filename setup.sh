1af9d719e1bf403f000be48bf1743b43b00d3ff5
ZONGXIA LI <zongxiali@ZONGXIAs-MacBook-Pro.local>
2026-02-20 13:40:25 -0500

    setup script

---
+++ setup.sh
@@ -0,0 +1,18 @@
+conda create -n prometheus python=3.11 
+conda activate prometheus
+
+# For CUDA 12.1 (most common on modern servers)
+pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu121
+
+# For CUDA 12.4
+pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu124
+
+cd ./gemma_train
+
+pip install -e .
+
+pip install deepspeed
+
+bash run_gemma2_prometheus.sh
+
+bash run_gemma2_prometheus.sh
